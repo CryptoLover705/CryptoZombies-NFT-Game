3ec3249f25893f3b9f1524b101ec85aa803e94ed
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2021-08-06 08:44:15 +0100

    update

--- client/index.js
+++ client/index.js
@@ -1,123 +1,138 @@
 var web3js = new Web3(Web3.givenProvider);
+const provider = window.ethereum;
+
+
 var cryptoZombies;
 var userAccount;
+    
+    // const Contract = require('web3-eth-contract');
 
-async function startApp() {
-    var cryptoZombiesAddress = "YOUR_CONTRACT_ADDRESS";
-    let buildData = await $.getJSON('../build/contracts/ZombieOwnership.json');
-    cryptoZombies = new web3js.eth.Contract(buildData.abi, cryptoZombiesAddress);
+    // // set provider for all later instances to use
+    // Contract.setProvider('ws://localhost:8546');
+    
+    async function startApp() {
+        await provider.request({ method: "wallet_requestPermissions",
+        params: [
+            {
+                "eth_accounts": {}
+            }
+        ],
+        id: 0, 
+       });
+       await provider.request({ method: "eth_accounts",
+       params: [],
+       id: 0, 
+       });
+      var cryptoZombiesAddress = "0x1A9b1Fb78b8db2e7916007F3b3b774Fa79e7A054";
+      cryptoZombies = new web3js.eth.Contract(cryptoZombiesABI, cryptoZombiesAddress);
+      
 
-    var accountInterval = setInterval(function () {
-        // Check if account has changed
+      let accountInterval = setInterval(function () {
+        
         if (web3.eth.accounts[0] !== userAccount) {
-            userAccount = web3.eth.accounts[0];
-            // Call some function to update the UI with the new account
-            getZombiesByOwner(userAccount).then(displayZombies);
+          userAccount = web3.eth.accounts[0];
+
+          getZombiesByOwner(userAccount)
+            .then(displayZombies);
         }
-    }, 100);
+      }, 5000);
 
-    // Use `filter` to only fire this code when `_to` equals `userAccount`
-    cryptoZombies.events.Transfer({ filter: { _to: userAccount } })
+      cryptoZombies.events.Transfer({ filter: { _to: userAccount } })
         .on("data", function (event) {
-            let data = event.returnValues;
-            // The current user just received a zombie!
-            // Do something here to update the UI to show it
-            getZombiesByOwner(userAccount).then(displayZombies);
+          let data = event.returnValues;
+          getZombiesByOwner(userAccount).then(displayZombies);
         }).on("error", console.error);
-}
+    }
+    
+    function displayZombies(ids) {
+      $("#zombies").empty();
+      for (id of ids) {
 
-function displayZombies(ids) {
-    // Start here
-    $('#zombies').empty();
-    for (id of ids) {
-        getZombieDetails(id).then(function (zombie) {
+        getZombieDetails(id)
+          .then(function (zombie) {
+
+
             $("#zombies").append(`<div class="zombie">
-          <ul>
-            <li>Name: ${zombie.name}</li>
-            <li>DNA: ${zombie.dna}</li>
-            <li>Level: ${zombie.level}</li>
-            <li>Wins: ${zombie.winCount}</li>
-            <li>Losses: ${zombie.lossCount}</li>
-            <li>Ready Time: ${zombie.readyTime}</li>
-          </ul>
-        </div>`);
-        })
+              <ul>
+                <li>Name: ${zombie.name}</li>
+                <li>DNA: ${zombie.dna}</li>
+                <li>Level: ${zombie.level}</li>
+                <li>Wins: ${zombie.winCount}</li>
+                <li>Losses: ${zombie.lossCount}</li>
+                <li>Ready Time: ${zombie.readyTime}</li>
+              </ul>
+            </div>`);
+          });
+      }
     }
-}
 
-function createRandomZombie(name) {
-    // This is going to take a while, so update the UI to let the user know
-    // the transaction has been sent
-    $("#txStatus").text("Creating new zombie on the blockchain. This may take a while...");
-    // Send the tx to our contract:
-    return cryptoZombies.methods.createRandomZombie(name)
+    function createRandomZombie(name) {
+
+
+      $("#txStatus").text("Creating new zombie on the blockchain. This may take a while...");
+
+      return cryptoZombies.methods.createRandomZombie(name)
         .send({ from: userAccount })
         .on("receipt", function (receipt) {
-            $("#txStatus").text("Successfully created " + name + "!");
-            // Transaction was accepted into the blockchain, let's redraw the UI
-            getZombiesByOwner(userAccount).then(displayZombies);
+          $("#txStatus").text("Successfully created " + name + "!");
+
+          getZombiesByOwner(userAccount).then(displayZombies);
         })
         .on("error", function (error) {
-            // Do something to alert the user their transaction has failed
-            $("#txStatus").text(error);
-        });
-}
 
-function feedOnKitty(zombieId, kittyId) {
-    // This is going to take a while, so update the UI to let the user know
-    // the transaction has been sent
-    $("#txStatus").text("Eating a kitty. This may take a while...");
-    // Send the tx to our contract:
-    return cryptoZombies.methods.feedOnKitty(zombieId, kittyId)
+          $("#txStatus").text(error);
+        });
+    }
+
+    function feedOnKitty(zombieId, kittyId) {
+      $("#txStatus").text("Eating a kitty. This may take a while...");
+      return cryptoZombies.methods.feedOnKitty(zombieId, kittyId)
         .send({ from: userAccount })
         .on("receipt", function (receipt) {
-            $("#txStatus").text("Ate a kitty and spawned a new Zombie!");
-            // Transaction was accepted into the blockchain, let's redraw the UI
-            getZombiesByOwner(userAccount).then(displayZombies);
+          $("#txStatus").text("Ate a kitty and spawned a new Zombie!");
+          getZombiesByOwner(userAccount).then(displayZombies);
         })
         .on("error", function (error) {
-            // Do something to alert the user their transaction has failed
-            $("#txStatus").text(error);
+          $("#txStatus").text(error);
         });
-}
+    }
 
-function levelUp(zombieId) {
-    $("#txStatus").text("Leveling up your zombie. This may take a while...");
-    return cryptoZombies.methods.levelUp(zombieId)
-        .send({ from: userAccount, value: web3js.utils.toWei("0.1", "BNB") })
+    function levelUp(zombieId) {
+      $("#txStatus").text("Leveling up your zombie...");
+      return cryptoZombies.methods.levelUp(zombieId)
+        .send({ from: userAccount, value: web3.utils.toWei("0.001", "ether") })
         .on("receipt", function (receipt) {
-            $("#txStatus").text("Power overwhelming! Zombie successfully leveled up!");
+          $("#txStatus").text("Power overwhelming! Zombie successfully leveled up");
         })
         .on("error", function (error) {
-            $("#txStatus").text(error);
+          $("#txStatus").text(error);
         });
-}
+    }
 
-function getZombieDetails(id) {
-    return cryptoZombies.methods.zombies(id).call();
-}
+    function getZombieDetails(id) {
+      return cryptoZombies.methods.zombies(id).call()
+    }
 
-function zombieToOwner(id) {
-    return cryptoZombies.methods.zombieToOwner(id).call();
-}
+    function zombieToOwner(id) {
+      return cryptoZombies.methods.zombieToOwner(id).call()
+    }
 
-function getZombiesByOwner(owner) {
-    return cryptoZombies.methods.getZombiesByOwner(owner).call();
-}
+    function getZombiesByOwner(owner) {
+      return cryptoZombies.methods.getZombiesByOwner(owner).call()
+    }
 
-$(document).ready(async () => {
+    window.addEventListener('load', function () {
 
 
-    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined') {
-        // Use Mist/MetaMask's provider
+      if (typeof web3 !== 'undefined') {
+
         web3js = new Web3(web3.currentProvider);
-    } else {
-        // Handle the case where the user doesn't have Metamask installed
-        // Probably show them a message prompting them to install Metamask
-        console.error('no web3 provider!');
-    }
+      } else {
 
-    // Now you can start your app & access web3 freely:
-    startApp();
-});
+
+      }
+
+
+      startApp()
+
+    })
--- truffle-config.js
+++ truffle-config.js
@@ -19,6 +19,8 @@
  */
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const dotenv = require('dotenv');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 const result = dotenv.config();
 if (result.error) {
@@ -38,28 +40,20 @@ exports.networks = {
    port: 8545,            // Standard Ethereum port (default: none)
    network_id: "97",       // Any network (default: none)
   },
-  // Another network with more advanced options...
-  // advanced: {
-  // port: 8777,             // Custom port
-  // network_id: 1342,       // Custom network
-  // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-  // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-  // from: <address>,        // Account to send txs from (default: accounts[0])
-  // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-  // },
-  // Useful for deploying to a public network.
-  // NB: It's important to wrap the provider as a function.
-  // bsc: {
-  //   provider: () => new HDWalletProvider(
-  //     process.env.MNEMONIC,
-  //     `https://speedy-nodes-nyc.moralis.io/a2bb21ef32b996eba7a5db09/bsc/mainnet`
-  //   ),
-  //   network_id: 97,
-  //   gas: 5500000,
-  //   confirmations: 2,
-  //   timeoutBlocks: 200,
-  //   skipDryRun: true,
-  // },
+  testnet: {
+    provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+    network_id: 97,
+    confirmations: 10,
+    timeoutBlocks: 200,
+    skipDryRun: true
+  },
+  bsc: {
+    provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+    network_id: 56,
+    confirmations: 10,
+    timeoutBlocks: 200,
+    skipDryRun: true
+  },
 };
 exports.mocha = {
   // timeout: 100000
