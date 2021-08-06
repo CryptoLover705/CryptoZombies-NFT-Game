b6d31b547be7007d524cdde0f44373082b59cf72
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2021-08-06 17:30:10 +0100

    update

--- contracts/ZombieOwnership.sol
+++ contracts/ZombieOwnership.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.5.0 <0.6.0;
 
 import "./ZombieAttack.sol";
 import "./ERC721.sol";
-
+ 
 /*
    @title Manages transferring zombie ownership
    @author mayjer
--- truffle-config.js
+++ truffle-config.js
@@ -19,8 +19,8 @@
  */
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const dotenv = require('dotenv');
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 const result = dotenv.config();
 if (result.error) {
@@ -41,14 +41,14 @@ exports.networks = {
    network_id: "97",       // Any network (default: none)
   },
   testnet: {
-    provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+    provider: () => new HDWalletProvider(MNEMONIC, `https://data-seed-prebsc-1-s1.binance.org:8545`),
     network_id: 97,
     confirmations: 10,
     timeoutBlocks: 200,
     skipDryRun: true
   },
   bsc: {
-    provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+    provider: () => new HDWalletProvider(MNEMONIC, `https://bsc-dataseed1.binance.org`),
     network_id: 56,
     confirmations: 10,
     timeoutBlocks: 200,
@@ -60,6 +60,6 @@ exports.mocha = {
 };
 exports.compilers = {
   solc: {
-    version: '0.5.12',
+    version: '0.4.25',
   },
 };
